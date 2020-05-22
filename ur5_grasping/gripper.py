2574742e2ad69f3f7caf812a1b2433c03c521aec
portgasray <137861449@qq.com>
2020-05-22 16:00:14 +0800

    update param control

--- ur5_grasping/gripper.py
+++ ur5_grasping/gripper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import threading
 import socket
 from threading import *
@@ -25,6 +26,7 @@ class GripperClient:
         client.send(b"set_digital_out(0,True)\n")
         data  = client.recv(1024)
         client.close()
+        print("gripper oFF")
 
     def close(self):
         """
@@ -35,8 +37,15 @@ class GripperClient:
         client.send(b"set_digital_out(0,False)\n")
         data  = client.recv(1024)
         client.close()
+        print("gripper ON")
 
 
 if __name__ == "__main__":
+    print("with argument \'close\' or \'open\'")
     gripper = GripperClient()
-    gripper.close()
+    if str(sys.argv[1]) == "open":
+        gripper.open()
+    elif str(sys.argv[1]) == 'close':
+        gripper.close()
+    else:
+        print("Input invaild")
